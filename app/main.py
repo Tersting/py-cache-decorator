58fdb50352a9045dd916d741419e172842118768
Nazar <your@mail.com>
2025-09-06 12:27:44 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,19 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    memory_dict = {}
+
+    def inner(*args, **kwargs) -> Any:
+        key = (*args, tuple(sorted(kwargs.items())))
+
+        if key in memory_dict:
+            print("Getting from cache")
+            return memory_dict[key]
+        else:
+            print("Calculating new result")
+            result = func(*args, **kwargs)
+            memory_dict[key] = result
+            return result
+
+    return inner
